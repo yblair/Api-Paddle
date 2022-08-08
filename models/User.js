d1e10f29e3ab0b7f5c708a842116777678812c77
PaulAndia <paulandresandia@gmail.com>
2022-08-08 16:20:18 -0400

    jwt2 checked

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const bcrypt = require("bcrypt")
 
 const userSchema = new Schema(
   {
@@ -54,6 +55,12 @@ userSchema.set('toJSON', {
     returnedObject.id = returnedObject._id
     delete returnedObject._id
   }
+})
+
+userSchema.pre('save', async function(next){
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
 const User = model('user', userSchema)
